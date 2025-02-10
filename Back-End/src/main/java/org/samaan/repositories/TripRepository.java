8bacbc2c2864b4d8524c5ef94028bf51b1ed4971
1204Vraj <141605713+1204Vraj@users.noreply.github.com>
2025-02-10 20:58:01 +0530

    Add files via upload

---
+++ Back-End/src/main/java/org/samaan/repositories/TripRepository.java
@@ -0,0 +1,12 @@
+package org.samaan.repositories;
+
+import org.samaan.model.Trip;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TripRepository extends MongoRepository<Trip, String> {
+    List<Trip> findByUserId(String userId);
+}
+
