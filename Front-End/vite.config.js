a8de6b0f0327e987e077974112ee8fcb7a7b0df7
Preet Brahmbhatt <preet.brahmbhatt.code@gmail.com>
2025-03-13 01:04:07 +0530

    Reconfigured some files

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // Use Render's assigned port
+    host: "0.0.0.0" // Allow external access
+  },
   define: {
     global: "window", // Fix SockJS issue
   },
