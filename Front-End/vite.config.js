950cf47d7dcfda73a79f3ca49a6c56d6642def83
Preet Brahmbhatt <138224316+preet1694@users.noreply.github.com>
2025-03-13 01:37:56 +0530

    Update vite.config.js

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -4,13 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.PORT) || 3000, // Bind to Render's dynamic port
+    port: process.env.PORT || 3000, // Ensure it picks up Render's dynamic port
     host: "0.0.0.0", // Allow external access
   },
   preview: {
-    port: parseInt(process.env.PORT) || 3000,
-    host: "0.0.0.0", // Allow Render to serve the app
-    allowedHosts: ["samaan.onrender.com"], // Allow your domain
+    port: process.env.PORT || 3000, // Ensure it picks up Render's dynamic port
+    host: "0.0.0.0", // Make sure it's externally accessible
   },
   define: {
     global: "window",
